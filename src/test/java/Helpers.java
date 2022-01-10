854956e6d0de470648c98538fb54cf67e9f3b16e
Angel <a.v.iliev13@gmail.com>
2022-01-10 10:47:56 +0200

    Fixed login helper

--- src/test/java/Helpers.java
+++ src/test/java/Helpers.java
@@ -1,69 +1,33 @@
 import static io.restassured.RestAssured.baseURI;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.StringJoiner;
+import static io.restassured.RestAssured.*;
 
-import io.restassured.RestAssured;
-import io.restassured.RestAssured.*;
-
-import org.json.simple.JSONObject;
-
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.junit.runner.Request;
-
 import static org.hamcrest.Matchers.*;
 
 public class Helpers {
 
-	public static Map<String, String> login(String credentials) throws IOException {
+	public static Map<String, String> login(String credentials) {
 		// TODO: maybe define this once on the class ??
 		baseURI = "https://dev11.nymbus.com/coreweb/controller/";
 
-		URL url = new URL(baseURI+ "login");
-		URLConnection con = url.openConnection();
-		HttpURLConnection http = (HttpURLConnection)con;
-		http.setRequestMethod("POST"); // PUT is another valid option
-		http.setDoOutput(true);
-
-		Map<String,String> arguments = new HashMap<>();
-		arguments.put("login", "evaidakaviciene");
-		arguments.put("password", "tfHL9tEEc5KTmkJJV5ks"); // This is a fake password obviously
-		StringJoiner sj = new StringJoiner("&");
-		for(Map.Entry<String,String> entry : arguments.entrySet())
-			sj.add(URLEncoder.encode(entry.getKey(), "UTF-8") + "="
-					+ URLEncoder.encode(entry.getValue(), "UTF-8"));
-		byte[] out = sj.toString().getBytes(StandardCharsets.UTF_8);
-		int length = out.length;
-
-		http.setFixedLengthStreamingMode(length);
-		http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-		http.connect();
-		try(OutputStream os = http.getOutputStream()) {
-			os.write(out);
-		}
-
-		System.out.println(http.getInputStream());
-
-		Map<String, String> params = new HashMap<String,String>();
-		params.put("body", credentials);
-//
-//		Request request = RestAssured..request()
+		Response response = 
+				given()
+					.body(credentials)
+				.when()
+					.post("/lotto")
+				.then()
+					.statusCode(200)
+					.body("success", equalTo(true))
+					.extract()
+					.response();
 		
-		Response response = RestAssured.post("https://dev11.nymbus.com/coreweb/controller/login", credentials);
-		assert response.statusCode() == 200;
-//		assert response.body("success", equalTo(true));
-		
-		return response.getCookies();		
+		// TODO: what happens when statusCode is not 200 or success is False?
+		return response.cookies();		
+	}
+	
+	public static String getUserId(Map<String, String> cookies) {
+		return "208329";
 	}
 }
--- src/test/java/Test_APIs.java
+++ src/test/java/Test_APIs.java
@@ -1,34 +1,22 @@
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
-import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
 import io.restassured.http.ContentType;
 
-import java.io.IOException;
 import java.util.Map;
 
 public class Test_APIs {
 	String credentials = "login=evaidakaviciene&password=tfHL9tEEc5KTmkJJV5ks";
-	/* 
-	 * TODO: separate this out to a helper function that can be used as `Test Setup` routine
-	 * It needs to return the cookies needed for requesting data from other endpoints 
-	 */	
+	
 	@Test
 	public void test_login() {
 		// TODO: maybe define this once on the class ??
 		baseURI = "https://dev11.nymbus.com/coreweb/controller/";
 		
-		/* TODO: Access and return cookies so that they can be used for future requests
-		 * Set-Cookie			mscore.cacheid=1639045350603; Path=/
-		 * Set-Cookie			JSESSIONID=0000MQArR-YNwmMQ-NzGQn-uvXY:-1; Path=/; HttpOnly
-		 */
-
-		
-		// TODO: Cookies exist even on this request? Should we ignore them or ???
 		given()
-				.contentType(ContentType.URLENC)
+			.contentType(ContentType.URLENC)
 			.body(credentials)
 		.when()
 			.post("/login")
@@ -38,36 +26,15 @@ public class Test_APIs {
 			.log().everything();
 	}
 	
-	public void test_getUserId() {
-		/* TODO: Need user ID so we can request stuff
-		 *  https://dev11.nymbus.com/coreweb/controller/context.getContext
-		 *  user	Object { id: 588, name: "Elmyra Vaidakaviciene", code: "evaidakaviciene", � }
-		 */
-		
-		/* TODO: Maybe this is better for username
-		 * https://dev11.nymbus.com/coreweb/controller/widget.Query?name=username
-		 * field	Object { USERFNAME: "Elmyra", USERID: "588", USERLNAME: "Vaidakaviciene" }
-		 */
-		
-		/* TODO: Where does this ID come from 208329??? -> This seems to be an account ID
-		 * https://dev11.nymbus.com/coreweb/controller/formData/CustomerCRM/?id=208329
-		 */
-	}
-	
-	// TODO: This test should check account numbers only 
 	@Test
-	public void test_CustomerCRM() throws IOException {
+	public void test_CustomerCRM() {
 		baseURI = "https://dev11.nymbus.com/coreweb/controller/";
 		Map<String, String> cookies =  Helpers.login(credentials);
-		// TODO: get these from login response
-		String sessionId = "0000-k8xZ7FoCk2XsrzS3ZRM4WD:-1";
-		String cacheId = "1639045350603";
+		String userId = Helpers.getUserId(cookies);
 		
 		given()
-//			.cookie("JSESSIONID", sessionId)
-//			.cookie("mscore.cacheid", cacheId)
-				.cookies(cookies)
-			.param("id", "208329") // TODO: Where does this ID come from?
+			.cookies(cookies)
+			.param("id", userId)
 		.when()
 			.get("/formData/CustomerCRM/")
 		.then()
