a8bb47e8348d8ef8b20414a1bd268f48dcb1fde2
Angel <a.v.iliev13@gmail.com>
2022-01-11 15:19:12 +0200

    New version

--- src/test/java/Helpers.java
+++ src/test/java/Helpers.java
@@ -1,5 +1,5 @@
-import static io.restassured.RestAssured.baseURI;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import static io.restassured.RestAssured.*;
 
@@ -31,5 +31,78 @@ public class Helpers {
 	
 	public static String getUserId(Map<String, String> cookies) {
 		return "208329";
+	}
+	
+
+	private static String getCustomerId(String userId, Map<String, String> cookies) {
+		List<String> response =
+			given()
+				.cookies(cookies)
+				.param("id", userId)
+			.when()
+				.get("/formData/CustomerCRM/")
+			.then()
+				.statusCode(200)
+				.body("success", equalTo(true))
+				.body("data.field.accountid", hasItems(userId))
+				.body("data.field.accountnumber", hasItems("55889"))
+				.log().all()
+				.extract().path("data.field.accountid");
+
+		return response.get(0);
+	}
+	
+	public static String getFirstAccountId(String userId, Map<String, String> cookies) {
+		String body = "{" +
+				"\"ruleType\":\"BankingCORE\"," +
+				"\"actions\":[" +
+				"\"request\"," +
+				"\"getCustomerAccounts\"" +
+				"]," +
+				"\"beans\":[" +
+				"{" +
+				"\"type\":\"bank.data.actmst\"," +
+				"\"fields\":{" +
+				"\"count\":999," +
+				"\"customerId\":\"" + getCustomerId(userId, cookies) + "\"" +
+				"}" +
+				"}" +
+				"]" +
+				"}";
+		List response = given()
+				.cookies(cookies)
+				.contentType(ContentType.JSON)
+				.body(body)
+				.when()
+				.post("widget._GenericProcess")
+				.then()
+				.statusCode(200)
+				.log().all()
+				.extract().path("data.fieldValue.resultList.id");
+
+		return ((ArrayList)response.get(0)).get(0).toString();
+	}
+	
+	public static String makeAccountTransactionRequest(String accountId) {
+		return "{" +
+				"\"ruleType\":\"BankingCORE\","+
+                "\"actions\":[" +
+                "\"request\"," +
+                "\"getAccountTransactions\"" +
+                "]," +
+                "\"beans\":[" +
+                "{" +
+                "\"type\":\"bank.data.actmst\"," +
+                "\"fields\":{" +
+                "\"accountId\":\"" + accountId + "\"," +
+                "\"count\":10," +
+                "\"offset\":0," +
+                "\"isWarehouseStatement\":false," +
+                "\"isItemsToWorkStatement\":false," +
+                "\"escrow\":0" +
+                "}" +
+                "}" +
+                "]" +
+                "}";
 	}
 }
--- src/test/java/Test_APIs.java
+++ src/test/java/Test_APIs.java
@@ -29,7 +29,7 @@ public class Test_APIs {
 		.then()
 			.statusCode(200)
 			.body("success", equalTo(true))
-			.log().everything();
+			.log().all();
 	}
 
 	@Test
@@ -37,7 +37,7 @@ public class Test_APIs {
 		Map<String, String> cookies =  Helpers.login(credentials);
 		String userId = Helpers.getUserId(cookies);
 
-		List response = given()
+		given()
 			.cookies(cookies)
 			.param("id", userId)
 		.when()
@@ -47,8 +47,7 @@ public class Test_APIs {
 			.body("success", equalTo(true))
 			.body("data.field.accountid", hasItems(userId))
 			.body("data.field.accountnumber", hasItems("55889"))
-			.log().all()
-				.extract().path("data.field.accountid");
+			.log().all();
 	}
 
 
@@ -56,27 +55,8 @@ public class Test_APIs {
 	public void test_getAccountTransactions() {
 		Map<String, String> cookies =  Helpers.login(credentials);
 		String userId = Helpers.getUserId(cookies);
-
-		String body = "{" +
-				"\"ruleType\":\"BankingCORE\","+
-                "\"actions\":[" +
-                "\"request\"," +
-                "\"getAccountTransactions\"" +
-                "]," +
-                "\"beans\":[" +
-                "{" +
-                "\"type\":\"bank.data.actmst\"," +
-                "\"fields\":{" +
-                "\"accountId\":\"" + getFirstAccountId(userId, cookies) + "\"," +
-                "\"count\":10," +
-                "\"offset\":0," +
-                "\"isWarehouseStatement\":false," +
-                "\"isItemsToWorkStatement\":false," +
-                "\"escrow\":0" +
-                "}" +
-                "}" +
-                "]" +
-                "}";
+		String accountId = Helpers.getFirstAccountId(userId, cookies);
+		String body = Helpers.makeAccountTransactionRequest(accountId);
 
 		given()
 			.cookies(cookies)
@@ -89,53 +69,6 @@ public class Test_APIs {
 			.log().all();
 	}
 
-	private String getFirstAccountId(String userId, Map<String, String> cookies) {
-		String body = "{" +
-				"\"ruleType\":\"BankingCORE\"," +
-				"\"actions\":[" +
-				"\"request\"," +
-				"\"getCustomerAccounts\"" +
-				"]," +
-				"\"beans\":[" +
-				"{" +
-				"\"type\":\"bank.data.actmst\"," +
-				"\"fields\":{" +
-				"\"count\":999," +
-				"\"customerId\":\"" + getCustomerId(userId, cookies) + "\"" +
-				"}" +
-				"}" +
-				"]" +
-				"}";
-		List response = given()
-				.cookies(cookies)
-				.contentType(ContentType.JSON)
-				.body(body)
-				.when()
-				.post("widget._GenericProcess")
-				.then()
-				.statusCode(200)
-				.log().all()
-				.extract().path("data.fieldValue.resultList.id");
+	
 
-		return ((ArrayList)response.get(0)).get(0).toString();
-	}
-
-
-	private String getCustomerId(String userId, Map<String, String> cookies) {
-		List<String> response =
-			given()
-				.cookies(cookies)
-				.param("id", userId)
-			.when()
-				.get("/formData/CustomerCRM/")
-			.then()
-				.statusCode(200)
-				.body("success", equalTo(true))
-				.body("data.field.accountid", hasItems(userId))
-				.body("data.field.accountnumber", hasItems("55889"))
-				.log().all()
-				.extract().path("data.field.accountid");
-
-		return response.get(0);
-	}
 }
