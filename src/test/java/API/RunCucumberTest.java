3da04fc857fd1bf6a694f4c0323311e32e0a9c21
Angel <a.v.iliev13@gmail.com>
2022-01-16 21:12:36 +0200

    remove wrong property

--- src/test/java/API/RunCucumberTest.java
+++ src/test/java/API/RunCucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(features = "src/test/java/API/UserScenarios.feature", format = {"pretty"})
+@CucumberOptions(features = "src/test/java/API/UserScenarios.feature")
 public class RunCucumberTest extends AbstractTestNGCucumberTests{
 
     @Override
